d1b6f3f0162117f714ba820a61793528c4b96179
void <ryanmckeown82@gmail.com>
2018-06-27 15:02:53 +0800

    oof
    travis is gay

---
+++ checks.py
@@ -0,0 +1,9 @@
+import sys
+
+#does this support decimals py3.6 best version :weary::tm:
+if sys.version_info[0] < 3:
+    raise Exception('Must be running Python version 3 or up')
+
+#ok do we need anything else
+#i think not
+#lets run haah yes
